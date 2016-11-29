bad09d1b46849c71d90519c7f2965f7a10fde4ad
awanderlyss <alyssa.felix7@gmail.com>
2016-11-29 18:26:06 -0500

    Create div els for each lettter, append to wordField in html

--- main.js
+++ main.js
@@ -3,21 +3,31 @@ $(function(){
 
     let userWord;
     let letterArr = [];
+    let wordFieldDiv = $('#wordField');
+
     // Onclick set userWord to input value, then clear field
     $('button').click(function(){
-        userWord = $('input[name=word]').val();
-        // Clear input field after click
+        userWord = $('input[name=word]').val().toUpperCase();
+        // Clear the current elements that have values
         $('input[name=word]').val("");
+        wordFieldDiv.text(" ");
+        letterArr = [];
+
         createLetterHolders(userWord);
     });
 
     // Create letterholder based on length of word
     function createLetterHolders(userWord) {
         for(let i = 0; i < userWord.length; i++) {
-            // Store each letter in an array
+            // Store each letter in letterArr
             letterArr[i] = userWord[i];
+            // Create a div el w/ 2 els p & span
+            let letterHolderDiv = $(`<div class="letterHolder">
+                <p class="letter">${userWord[i]}</p><span></span>
+                </div>`);
+            // Append letterHolderDiv to the wordFieldDiv
+            wordFieldDiv.append(letterHolderDiv);
         }
-        console.log(letterArr);
     }
     // Add array item to the div text field
 
