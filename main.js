54e7a785f9eda2e16180b04d74c636e8ce86cff1
awanderlyss <alyssa.felix7@gmail.com>
2016-11-29 15:40:14 -0500

    Add input, button to html and onclick func store input value in java

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-$('document').ready(
-    console.log('Js is Connected!')
+// Shorthand for $( document ).ready()
+$(function(){
 
-);
+    let userWord;
+
+    // Onclick set userWord to input value, then clear field
+    $('button').click(function(){
+        userWord = $('input[name=word]').val();
+        // Clear input field after click
+        $('input[name=word]').val("");
+        console.log(userWord);
+    });
+    // Splice the word to single letters and store into an array
+    
+    // Create "empty" divs for the length of array
+
+    // Add array item to the div text field
+
+    // Append every div to the wordField div
+
+    //
+
+});
