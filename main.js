52a4ff68e4f82723f13e2f73bee58383bb6dc77e
awanderlyss <alyssa.felix7@gmail.com>
2016-11-29 16:16:08 -0500

    Add func to iterate through the user word and store letter in array

--- main.js
+++ main.js
@@ -2,18 +2,23 @@
 $(function(){
 
     let userWord;
-
+    let letterArr = [];
     // Onclick set userWord to input value, then clear field
     $('button').click(function(){
         userWord = $('input[name=word]').val();
         // Clear input field after click
         $('input[name=word]').val("");
-        console.log(userWord);
+        createLetterHolders(userWord);
     });
-    // Splice the word to single letters and store into an array
-    
-    // Create "empty" divs for the length of array
 
+    // Create letterholder based on length of word
+    function createLetterHolders(userWord) {
+        for(let i = 0; i < userWord.length; i++) {
+            // Store each letter in an array
+            letterArr[i] = userWord[i];
+        }
+        console.log(letterArr);
+    }
     // Add array item to the div text field
 
     // Append every div to the wordField div
