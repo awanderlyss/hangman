100d6dd6c0800e29b891d2d3fd33cc1d4da53a62
awanderlyss <alyssa.felix7@gmail.com>
2016-11-29 20:39:30 -0500

    Create alphabet board for user to guess letters

--- main.js
+++ main.js
@@ -4,23 +4,31 @@ $(function(){
     let userWord;
     let letterArr = [];
     let wordFieldDiv = $('#wordField');
-
+    let userInputDiv = $('#userInput');
+    let gameStarted = false;
+    let button = $('button');
+    // Creates an array with the letters of the alphabet
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     // Onclick set userWord to input value, then clear field
-    $('button').click(function(){
-        userWord = $('input[name=word]').val().toUpperCase();
-        // Clear the current elements that have values
-        $('input[name=word]').val("");
-        wordFieldDiv.text(" ");
-        letterArr = [];
+    button.click(function(){
+        // Only runs code once to initiate the game
+        if (!gameStarted){
+            gameStarted = !gameStarted;
+            userWord = $('input[name=word]').val().toUpperCase();
+            // Change the text value of the button
+            button.text('Submit Letter');
 
-        createLetterHolders(userWord);
+            createLetterHolders(userWord);
+            createAlphabetBoard();
+        }
+
     });
 
     // Create letterholder based on length of word
     function createLetterHolders(userWord) {
         for(let i = 0; i < userWord.length; i++) {
             // Store each letter in letterArr
-            letterArr[i] = userWord[i];
+            letterArr.push(userWord[i]);
             // Create a div el w/ 2 els p & span
             let letterHolderDiv = $(`<div class="letterHolder">
                 <p class="letter">${userWord[i]}</p><div class="dash"></div>
@@ -29,10 +37,15 @@ $(function(){
             wordFieldDiv.append(letterHolderDiv);
         }
     }
-    // Add array item to the div text field
 
-    // Append every div to the wordField div
-
-    //
+    // Create alphabet board for user letter guess
+    function createAlphabetBoard(){
+        userInputDiv.html("");
+        for(let i in alphabet) {
+            let span = $('<span></span>');
+            span.text(alphabet[i].toUpperCase());
+            userInputDiv.append(span);
+        }
+    }
 
 });
